1c7a3512ce5aaf69ab4e7f9a256d978c00eb6563
Gunesh <atharvakulkarni889@gmail.com>
2025-01-30 22:11:15 +0530

    change

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const logoutButton = document.querySelectorAll('.button')[7]; 
+    const logoutButton = document.querySelector('.button:last-child'); 
 
-    logoutButton.addEventListener('click', function() {
-        const userConfirmation = confirm('Are you sure you want to log out?');
+    if (logoutButton) {
+        logoutButton.addEventListener('click', function() {
+            const userConfirmation = confirm('Are you sure you want to log out?');
 
-        if (userConfirmation) {
-            window.location.href = '../index-page/index.html'; 
-        } else {
-            
-            window.location.href = 'landing-page.html'; 
-        }
-    });
+            if (userConfirmation) {
+                window.location.href = '../index-page/index.html'; 
+            } else {
+                window.location.href = 'landing-page.html'; 
+            }
+        });
+    }
 });
